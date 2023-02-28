61097399d5ee57534d1f6ad1a0bbcc7a76fffbbf
Richard Cheney <richeney@microsoft.com>
2023-02-28 11:08:41 +0000

    Changed outputs from ACI to ACA

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
+output "ip_addresses" {
+  value = azurerm_container_app.example.outbound_ip_addresses
+}
+
+output "fqdn" {
+  value = azurerm_container_app.example.latest_revision_fqdn
+}
+
+/*
 output "ip_address" {
   value = azurerm_container_group.example.ip_address
 }
@@ -5,3 +14,4 @@ output "ip_address" {
 output "fqdn" {
   value = "http://${azurerm_container_group.example.fqdn}"
 }
+*/
