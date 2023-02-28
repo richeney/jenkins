bf017c1df456e7b8f83fedb31fc686b2bae11b06
Richard Cheney <richeney@microsoft.com>
2023-02-28 11:59:21 +0000

    Traffic weight

--- main.tf
+++ main.tf
@@ -35,6 +35,10 @@ resource "azurerm_container_app" "example" {
   ingress {
     external_enabled = true
     target_port      = 80
+
+    traffic_weight {
+      percentage = 100
+    }
   }
 
   template {
